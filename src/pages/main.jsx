79a7ccb9cd9a1c142d821b70075c69f5d73d2a04
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-06-26 15:25:44 +0530

    test

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -5,11 +5,12 @@ import React from 'react'
 
 
 const Main = () => {
-    const {data} = useFetch("https://randomuser.me/api/");
+    const {data} = useFetch("http://maps.openweathermap.org/maps/2.0/weather/TA2/10/-41.36384/114.57899?appid=363bb53f013659562bdd24040513481f");
     console.log(data);
 
     return (
         <div>
+            <p>{data}</p>
         </div>
     )
 }
