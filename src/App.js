62b3b9bf0b2cbd9effc32464fbeb72e154213847
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-06-26 14:35:15 +0530

    some minor work is done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import {BrowserRouter as Router , Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import MainPage from './pages/main'
 import './App.css';
 
 function App() {
-  
-  return (
-   <Router> 
-     <Route path="/">
-       <div>
-       <img src="http://ikazme.com/wp-content/uploads/2018/08/helloworld.png"/> 
-      </div> 
-     </Route>
-     <Route path ="/main" component={MainPage}/>
-   </Router>
-  );
+
+    return (
+        <Router>
+                <Route path="/" component={MainPage}/>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+//Fetching Data with Custom Hook
+    import useFetch from "react-fetch-hook"
+
+
 const Main = () => {
+    const {data} = useFetch("https://randomuser.me/api/");
+    console.log(data);
+
     return (
         <div>
-            mainpage uh!
         </div>
     )
 }
-
-export default Main
+export default Main;
